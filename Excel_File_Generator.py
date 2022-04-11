f70ad45b848a9b134b0c91cfb8e719abf8912c45
MUHAMMAD LUQMAN <luqmanbutt@outlook.com>
2022-04-12 01:34:55 +0500

    Update Excel_File_Generator.py

--- Excel_File_Generator.py
+++ Excel_File_Generator.py
@@ -26,29 +26,29 @@ st.header("EXCEL FILE GENERATOR WEB APP")
 st.text("With the help of this app you can convert your CSV file to Excel.")
 st.text("Finally app is working huhu!")
 
-# if uploaded_file is not None:
+if uploaded_file is not None:
 
-#     @st.cache
-#     def load_csv():
-#         csv = pd.read_csv(uploaded_file)
-#         return csv
-#     df = load_csv()
-#     st.write("Uploaded CSV File")
-#     st.write(df)
+    @st.cache
+    def load_csv():
+        csv = pd.read_csv(uploaded_file)
+        return csv
+    df = load_csv()
+    st.write("Uploaded CSV File")
+    st.write(df)
 
-#     def to_excel(df):
-#         output = BytesIO()
-#         writer = pd.ExcelWriter(output, engine='xlsxwriter')
-#         df.to_excel(writer, index=False, sheet_name='Sheet1')
-#         workbook = writer.book
-#         worksheet = writer.sheets['Sheet1']
-#         format1 = workbook.add_format({'num_format': '0.00'}) 
-#         worksheet.set_column('A:A', None, format1)  
-#         writer.save()
-#         processed_data = output.getvalue()
-#         return processed_data
-#     df_xlsx = to_excel(df)
-#     st.download_button(label='📥 Download Excel File', data=df_xlsx , file_name= 'workbook.xlsx')
+    def to_excel(df):
+        output = BytesIO()
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+        workbook = writer.book
+        worksheet = writer.sheets['Sheet1']
+        format1 = workbook.add_format({'num_format': '0.00'}) 
+        worksheet.set_column('A:A', None, format1)  
+        writer.save()
+        processed_data = output.getvalue()
+        return processed_data
+    df_xlsx = to_excel(df)
+    st.download_button(label='📥 Download Excel File', data=df_xlsx , file_name= 'workbook.xlsx')
 
-# else:
-#     st.info("Awaiting for file")
+else:
+    st.info("Awaiting for file")
